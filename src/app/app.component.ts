11228989d8f8c5a0dbc684ea8c47219e98cdbb91
mazen87 <mazen.karfoul87@hotmail.com>
2021-04-24 23:39:44 +0200

    adding navigation with event binding and ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipeBook-shoppingList-project';
+
+  route : string = 'recipe'; 
+
+  onNavigation(routeSelected :string){
+    this.route= routeSelected;
+  }
 }
